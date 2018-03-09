9d91c80751c5c32f356cad8f72aa4b86574375b6
Leo Yuanjie Li <leo.liyj@gmail.com>
2018-03-08 22:24:18 -0800

    Update server.js

--- server.js
+++ server.js
@@ -2,60 +2,42 @@
 
 const bodyParser = require('body-parser');
 const express = require('express');
+const morgan = require('morgan');
 const mongoose = require('mongoose');
-
-// Mongoose internally uses a promise-like object,
-// but its better to make Mongoose use built in es6 promises
 mongoose.Promise = global.Promise;
 
-// config.js is where we control constants for entire
-// app like PORT and DATABASE_URL
-const { PORT, DATABASE_URL } = require('./config');
-const { BlogPosts } = require('./models');
-
+const { DATABASE_URL, PORT } = require('./config');
+const { BlogPost } = require('./models');
 
 const app = express();
 
-const blogPostRouter = require('./blogRouter');
-
-
-// app.use(morgan('common'));
-app.use(express.static('public'));
+app.use(morgan('common'));
 app.use(bodyParser.json());
-app.use('/posts', blogPostRouter);
 
-app.get('/', (req, res) => {
-  // res.sendFile(__dirname + '/views/index.html');
-  BlogPosts
-  .find()
-  .then(function(posts){
-      res.json({
-        posts: posts.map(
-          (post) => post.serialize())
-      });
-  })
-  .catch(function(err){
-    console.error(err);
-    res.status(500).json({ message: 'Internal server error' });
-  })
+app.get('/posts', (req, res) => {
+  BlogPost
+    .find()
+    .then(posts => {
+      res.json(posts.map(post => post.serialize()));
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'something went terribly wrong' });
+    });
 });
 
-// can also request by ID
-app.get('/:id', (req, res) => {
-  BlogPosts
-    // this is a convenience method Mongoose provides for searching
-    // by the object _id property
+app.get('/posts/:id', (req, res) => {
+  BlogPost
     .findById(req.params.id)
     .then(post => res.json(post.serialize()))
     .catch(err => {
       console.error(err);
-      res.status(500).json({ message: 'Internal server error' });
+      res.status(500).json({ error: 'something went horribly awry' });
     });
 });
 
-app.post('/', (req, res) => {
-
-  const requiredFields = ['title', 'author', 'content'];
+app.post('/posts', (req, res) => {
+  const requiredFields = ['title', 'content', 'author'];
   for (let i = 0; i < requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
@@ -65,21 +47,23 @@ app.post('/', (req, res) => {
     }
   }
 
-  BlogPosts
+  BlogPost
     .create({
       title: req.body.title,
       content: req.body.content,
-      author: req.body.author,
+      author: req.body.author
     })
-    .then(post => res.status(201).json(post.serialize()))
+    .then(blogPost => res.status(201).json(blogPost.serialize()))
     .catch(err => {
       console.error(err);
-      res.status(500).json({ message: 'Internal server error' });
+      res.status(500).json({ error: 'Something went wrong' });
     });
+
 });
 
-app.delete('/:id', (req, res) => {
-  BlogPosts
+
+app.delete('/posts/:id', (req, res) => {
+  BlogPost
     .findByIdAndRemove(req.params.id)
     .then(() => {
       res.status(204).json({ message: 'success' });
@@ -90,7 +74,8 @@ app.delete('/:id', (req, res) => {
     });
 });
 
-app.put('/:id', (req, res) => {
+
+app.put('/posts/:id', (req, res) => {
   if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
     res.status(400).json({
       error: 'Request path id and request body id values must match'
@@ -105,14 +90,21 @@ app.put('/:id', (req, res) => {
     }
   });
 
-  BlogPosts
+  BlogPost
     .findByIdAndUpdate(req.params.id, { $set: updated }, { new: true })
     .then(updatedPost => res.status(204).end())
     .catch(err => res.status(500).json({ message: 'Something went wrong' }));
 });
 
 
-
+app.delete('/:id', (req, res) => {
+  BlogPost
+    .findByIdAndRemove(req.params.id)
+    .then(() => {
+      console.log(`Deleted blog post with id \`${req.params.id}\``);
+      res.status(204).end();
+    });
+});
 
 
 app.use('*', function (req, res) {
